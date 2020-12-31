83d604c25b7afa1c168e5d78fccbec6b636884b8
Nipun Garg <gargnipungarg@gmail.com>
2020-12-31 15:18:59 +0530

    parameter correction

--- vars/cloneSource.groovy
+++ vars/cloneSource.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(String source, String branch) {
-  git url: source, branch: branch, creds: globalVars.credsId
+  git url: source, branch: branch, credentialsId: globalVars.credsId
 }
