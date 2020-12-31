e4475db27ded0c361c881aa1a726dc3bc6210f23
Nipun Garg <gargnipungarg@gmail.com>
2020-12-31 15:05:03 +0530

    parameterized

--- src/CICD.groovy
+++ src/CICD.groovy
@@ -2,9 +2,12 @@
 library 'nipun-shared'
 
 def execute(body) {
+  print(body.SOURCE)
+  def src = body.SOURCE
+  def branch = body.BRANCH
   node {
     stage('Clone Source') {
-      cloneSource(body.SOURCE, body.BRANCH)
-    }
+      cloneSource(src, branch)
+    }// end stage clone source
   }
 }
